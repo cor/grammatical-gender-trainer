3ee7aecae095d18343353477ba25a34b22aedd10
Jurriaan Pruijs <jurriaan@pruijs.nl>
2015-02-02 20:57:48 +0100

    Initial Commit

---
+++ Grammatical Gender Trainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grammatical Gender Trainer
+//
+//  Created by Jurriaan Pruijs on 02-02-15.
+//  Copyright (c) 2015 Jurriaan Pruijs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
