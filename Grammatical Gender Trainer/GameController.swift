21b9cd8cc78d729192bc5f7da8eaa9950f20adbf
CorPruijs <cor@pruys.com>
2015-02-05 20:34:32 +0100

    removed redundant  Array extension

--- Grammatical Gender Trainer/GameController.swift
+++ Grammatical Gender Trainer/GameController.swift
@@ -8,15 +8,6 @@
 
 import UIKit
 
-extension Array {
-    mutating func shuffle() {
-        for i in 0..<(count - 1) {
-            let j = Int(arc4random_uniform(UInt32(count - i))) + i
-            swap(&self[i], &self[j])
-        }
-    }
-}
-
 class GameController: UIViewController {
     
     var words : [Word]!
