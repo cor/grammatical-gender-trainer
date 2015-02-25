19871fb106b0b407e979f4759e08e008c80230df
CorPruijs <cor@pruys.com>
2015-02-25 16:47:07 +0100

    upgraded game view; MOAR STATS

--- Grammatical Gender Trainer/GameController.swift
+++ Grammatical Gender Trainer/GameController.swift
@@ -10,17 +10,25 @@ import UIKit
 
 class GameController: UIViewController {
     
+    @IBOutlet weak var remainingWordsLabel: UILabel!
+    @IBOutlet weak var remainingRoundsLabel: UILabel!
+    @IBOutlet weak var timeRunningLabel: UILabel!
     @IBOutlet weak var currentWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var wordProgressBar: UIProgressView!
     
     var words: [Word]!
     var game: Game!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
         game = Game(words: words)
         game.start()
         updateView()
+        
+        NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector: Selector("updateTimeLabel"), userInfo: nil, repeats: true)
     }
     
     @IBAction func genderButtonPressed(sender: UIButton) {
@@ -44,10 +52,15 @@ class GameController: UIViewController {
             // update currentWord label
             currentWordLabel.text = game.currentWord?.word
             
+            scoreLabel.text = "\(game.score)"
+            remainingWordsLabel.text = "\(game.remainingWordsInRound)"
             wordProgressBar.setProgress((Float(1.0) - Float(game.remainingWordsInRound) / Float(game.wordSource.count)), animated: true)
             
         }
-        
+    }
+    
+    func updateTimeLabel() {
+        timeRunningLabel.text = String(format: "%.1f", game.time())
     }
     
     
